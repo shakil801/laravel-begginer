233511f0b60a5bf73ab2acd9620e7c3e6b695f72
shakil801 <personalweb924@gmail.com>
2024-05-30 14:38:57 +0600

    laravel-db

---
+++ laravel-db/app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function save(Request $request){
+        // dd('sdjasdsa');
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+        // Validate the request data
+    $validatedData = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+    ]);
+    $user = User::create([
+        'name' => $validatedData['name'],
+        'email' => $validatedData['email'],
+        'password' => Hash::make($validatedData['password']),
+    ]);
+    return redirect()->route('/')->with(['message' => 'User created successfully', 'user' => $user], 201);
+    }
+}
---
+++ laravel-db/resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        @if(Session('message'))
+        <p>{{Session('message')}}</p>
+        @endif
+        <form action="{{route('form.save')}}" method="post" class="mt-5">
+            @csrf
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" class="form-control" name="email">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="text" class="form-control" name="password">
+            </div>
+
+            <button type="submit" class="btn btn-warning">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
